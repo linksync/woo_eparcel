87e3c90eea9435b7dc5033bb5e0fc0f9dadeaaa7
Jameshwart Lopez <jameshwart@linksync.com>
2016-09-23 15:45:23 -0700

    version 1.1.4

--- linksynceparcel.php
+++ linksynceparcel.php
@@ -3,7 +3,7 @@
  * Plugin Name: linksync eParcel
  * Plugin URI: http://www.linksync.com/integrate/woocommerce-eparcel-integration
  * Description: Manage your eParcel orders without leaving your WordPress WooCommerce store with linksync eParcel for WooCommerce.
- * Version: 1.1.3
+ * Version: 1.1.4
  * Author: linksync
  * Author URI: http://www.linksync.com
  * License: GPLv2
